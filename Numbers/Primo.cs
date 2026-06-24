7a05595f3bb0afb96634139c2256cfaf366a50a5
Macsim <macsim.code@gmail.com>
2026-06-24 21:50:07 +0100

    Create Primo.cs

---
+++ Numbers/Primo.cs
@@ -0,0 +1,45 @@
+namespace MyApp
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite um numero");
+            int numero = int.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine($"O numero é primo: {Palindromo(numero)} ");
+        }
+
+
+
+
+            static bool Primo(int numero)
+            {
+               for (int i = 2; i < numero; i++)
+
+               {
+                   
+                 if(numero % i  == 0)
+                 {
+
+                    return false;
+                 }
+                 
+
+
+
+               }
+
+               return true;
+  
+            
+            }
+
+        
+
+    }   
+}
